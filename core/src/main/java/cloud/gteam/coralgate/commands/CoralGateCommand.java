aa0cc0bf64cb261166dfe2035b99034a380fa28d
XIII-MC <xiiimc@gteam.cloud>
2026-02-09 20:10:12 +0100

    Updated copyright Added version and help commands

--- core/src/main/java/cloud/gteam/coralgate/commands/CoralGateCommand.java
+++ core/src/main/java/cloud/gteam/coralgate/commands/CoralGateCommand.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of CoralGate - https://github.com/GTeamX/CoralGate
- * Copyright (C) 2025 GTeamX (GTeam) and it's contributors
+ * Copyright (C) 2026 GTeamX (GTeam) and it's contributors
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -22,6 +22,7 @@ import cloud.gteam.coralgate.CorePlugin;
 import cloud.gteam.coralgate.commands.permissions.CommandPermission;
 import cloud.gteam.coralgate.config.ConfigManager;
 import cloud.gteam.coralgate.config.ConfigModel;
+import com.github.retrooper.packetevents.PacketEvents;
 import revxrsal.commands.annotation.Command;
 import revxrsal.commands.annotation.Description;
 import revxrsal.commands.annotation.Subcommand;
@@ -36,6 +37,64 @@ public class CoralGateCommand {
 
     public CoralGateCommand(final CorePlugin corePlugin) {
         this.corePlugin = corePlugin;
+    }
+
+    // /coralgate|cg (help)
+    @Subcommand("help")
+    @Description("Show the help message for CoralGate.")
+    @CommandPermission("coralgate.commands.help")
+    public void help(final CommandActor actor) {
+
+        final ConfigManager configManager = this.corePlugin.getConfigManager();
+        final ConfigModel config = configManager.getConfig();
+
+        actor.sendRawMessage("§7-----------------------------------------------------");
+        actor.sendRawMessage("");
+        actor.sendRawMessage(config.getNormalPrefix() + "§f" + this.corePlugin.getPlatformProperties().getProperty("platform-version") + " | §7§o(" + this.corePlugin.getPlatformProperties().getProperty("core-version") + ")§r");
+        actor.sendRawMessage("§7Made by XIII___ and Vagdedes2 with(out) love!");
+        actor.sendRawMessage("");
+        actor.sendRawMessage("§7Available commands:");
+        actor.sendRawMessage("");
+        actor.sendRawMessage(" - /coralgate§8|cg§r help §7- Show this menu.");
+        actor.sendRawMessage(" - /coralgate§8|cg§r version§8|ver§r §7- Show versions.");
+        actor.sendRawMessage(" - /coralgate§8|cg§r config§8|cfg§r reload§8|rl§r §7- Reload the config.");
+        actor.sendRawMessage("");
+        actor.sendRawMessage("§bHaving troubles ? Need help ? Found a bug ?");
+        actor.sendRawMessage("§7Join our Discord: §bhttps://discord.gg/rxV89DZHEd");
+        actor.sendRawMessage("");
+        actor.sendRawMessage("§7-----------------------------------------------------");
+
+    }
+
+    // /coralgate|cg version|ver
+    @Subcommand({"version", "ver"})
+    @Description("Show different versions for CoralGate.")
+    @CommandPermission("coralgate.commands.version")
+    public void version(final CommandActor actor) {
+
+        final ConfigManager configManager = this.corePlugin.getConfigManager();
+        final ConfigModel config = configManager.getConfig();
+
+        final String currentVersion = this.corePlugin.getPlatformProperties().getProperty("core-version") + "_" + this.corePlugin.getPlatformProperties().getProperty("platform-version");
+
+        actor.sendRawMessage("§7-----------------------------------------------------");
+        actor.sendRawMessage("");
+        actor.sendRawMessage(config.getNormalPrefix() + "§fVersions information");
+        actor.sendRawMessage("");
+        actor.sendRawMessage("§7Platform: §f" + this.corePlugin.getPlatformProperties().getProperty("platform-name"));
+        actor.sendRawMessage("§7Version: " + (Objects.equals(this.corePlugin.getUpdateChecker().getLatestVersion(), currentVersion) ? "§a" : "§e") + this.corePlugin.getPlatformProperties().getProperty("platform-version"));
+        actor.sendRawMessage("");
+        actor.sendRawMessage("§7Core version: " + (Objects.equals(this.corePlugin.getUpdateChecker().getLatestVersion(), currentVersion) ? "§a" : "§e") + this.corePlugin.getPlatformProperties().getProperty("core-version"));
+        actor.sendRawMessage("");
+        actor.sendRawMessage("§7Configuration version: §f" + config.getConfigVersion());
+        actor.sendRawMessage("");
+        actor.sendRawMessage("§7API host: §f" + config.getApiHost());
+        actor.sendRawMessage("§7API version: §f" + config.getApiVersion());
+        actor.sendRawMessage("");
+        actor.sendRawMessage("§7packetevents version: " + (PacketEvents.getAPI().getVersion().toString().equals(this.corePlugin.getPlatformProperties().getProperty("packetevents-version")) ? "§a" : "§e") + this.corePlugin.getPlatformProperties().getProperty("packetevents-version"));
+        actor.sendRawMessage("");
+        actor.sendRawMessage("§7-----------------------------------------------------");
+
     }
 
     // /coralgate|cg config|cfg reload|rl.
