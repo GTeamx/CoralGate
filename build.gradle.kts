8d8e8daca0ae78276e29cd4039ef73a28d72f59f
XIII-MC <81795405+XIII-MC@users.noreply.github.com>
2026-06-04 22:03:19 +0200

    Updated versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,15 @@
 // Project related options
-extra["coreVersion"] = "0.3.0-beta"
+extra["coreVersion"] = "0.4.0-rc"
 
 // Dependency related options
 extra["lampVersion"] = "4.0.0-rc.16"
-extra["packetEventsVersion"] = "2.12.1"
+extra["packetEventsVersion"] = "2.12.2"
 extra["bstatsVersion"] = "3.2.1"
 
 subprojects {
 
     group = "cloud.gteam"
-    version = "0.3.0-beta"
+    version = "0.3.1-beta"
 
     repositories {
         mavenCentral()
