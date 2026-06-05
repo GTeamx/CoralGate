343f5e2dd106afe83ed303b487e1118d296d3ada
XIII-MC <81795405+XIII-MC@users.noreply.github.com>
2026-06-05 22:51:55 +0200

    Downgraded to AsyncHTTPClient 2.15.0 for Java 8 compatibility Enforcing java version

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -12,6 +12,10 @@ version = rootProject.extra["coreVersion"]!!
 
 java {
 
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(8))
+    }
+
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
 
@@ -36,7 +40,7 @@ dependencies {
     implementation("io.github.revxrsal:lamp.common:$lampVersion")
     implementation("blue.endless:jankson:1.2.3")
     compileOnly("com.google.code.gson:gson:2.14.0")
-    implementation("org.asynchttpclient:async-http-client:3.0.10")
+    implementation("org.asynchttpclient:async-http-client:2.15.0")
     implementation("org.jetbrains:annotations:26.1.0")
     implementation("dev.dejvokep:boosted-yaml:1.3.7")
 
