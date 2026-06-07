a5c060f682249dbb0ff0ae607dbb93f0054bf04e
XIII-MC <81795405+XIII-MC@users.noreply.github.com>
2026-06-07 21:08:02 +0200

    Bumped version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 // Project related options
-extra["coreVersion"] = "0.4.1-rc"
+extra["coreVersion"] = "0.4.2-SNAPSHOT"
 
 // Dependency related options
 extra["lampVersion"] = "4.0.0-rc.16"
@@ -9,7 +9,7 @@ extra["bstatsVersion"] = "3.2.1"
 subprojects {
 
     group = "cloud.gteam"
-    version = "0.3.0-rc"
+    version = "0.3.1-SNAPSHOT"
 
     repositories {
         mavenCentral()
