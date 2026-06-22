712e5afcdc74adeb5a32caca5baa451156f553df
XIII-MC <81795405+XIII-MC@users.noreply.github.com>
2026-06-22 18:10:54 +0200

    Updated config version

--- core/src/main/java/cloud/gteam/coralgate/config/ConfigManager.java
+++ core/src/main/java/cloud/gteam/coralgate/config/ConfigManager.java
@@ -31,7 +31,7 @@ import java.io.InputStream;
 
 public class ConfigManager {
 
-    private final String latestConfigVersion = "0.2.0";
+    private final String latestConfigVersion = "0.2.1";
 
     private final String configFileName;
     private final File configFile;
@@ -81,7 +81,7 @@ public class ConfigManager {
 
         if (this.document == null) return;
 
-        this.config.setConfigVersion(this.document.getString("version", "0.2.0"));
+        this.config.setConfigVersion(this.document.getString("version", "0.2.1"));
 
         this.config.setNormalPrefix(this.document.getString("prefixes.normal", "§b§lCoralGate §7» §r"));
         this.config.setWarningPrefix(this.document.getString("prefixes.warning", "§6§lCoralGate §7» §r"));
